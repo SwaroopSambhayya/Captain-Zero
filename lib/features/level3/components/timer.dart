eebecb1e6960ab6fb0838e24096268ded43568ea
swaroopsambhayya <swaroop.sambhayya@gmail.com>
2024-03-07 16:57:33 +0530

    Level 3

--- lib/features/level3/components/timer.dart
+++ lib/features/level3/components/timer.dart
@@ -1,11 +1,14 @@
 import 'dart:async';
 
 import 'package:captain_zero/features/level3/components/save_the_earth.dart';
+import 'package:captain_zero/features/level3/providers/audio_provider.dart';
 import 'package:flame/components.dart';
+import 'package:flame_riverpod/flame_riverpod.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class GameTimer extends PositionComponent with HasGameRef<SaveTheEarth> {
+class GameTimer extends PositionComponent
+    with HasGameRef<SaveTheEarth>, RiverpodComponentMixin {
   late Timer gameTimer;
   final TextPaint textPaint = TextPaint(
     style: const TextStyle(
@@ -29,6 +32,8 @@ class GameTimer extends PositionComponent with HasGameRef<SaveTheEarth> {
       gameTimer.stop();
       game.pauseEngine();
       game.routerContext.go('/levelComplete/3');
+      ref.read(audioProvider).stopMusic();
+      ref.read(audioProvider).playVictorySound();
     }
   }
 
